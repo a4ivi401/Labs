d61e699c795121ae2061204b33f9fe91f74990dc
a4ivi401 <a4ivi4.mail@gmail.com>
2026-02-21 00:57:58 -0800

    Migrate to VS 2026 and update .gitignore

--- lab_1/Program.cs
+++ lab_1/Program.cs
@@ -1,27 +1,32 @@
-﻿namespace lab_1;
+﻿using System.Numerics;
+using System.Globalization;
+
+namespace lab_1;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
         try
         {
             Console.Write("Введіть координату Х: ");
             double inputX = Convert.ToDouble(Console.ReadLine());
-            
+
             Console.Write("Введіть координату Y: ");
             double inputY = Convert.ToDouble(Console.ReadLine());
-            
+
             Vector vec = new Vector(inputX, inputY);
-            
+
             Console.WriteLine("---Дані обʼєкта---");
             Console.WriteLine($"Вектор: ({vec.X}, {vec.Y})");
-            
+
             double radius = vec.CalculateRadius();
             double angleRad = vec.CalculateAngle();
-            
+
             double angleDeg = angleRad * (180.0 / Math.PI);
-            
+
             Console.WriteLine("---Полярні координати---");
             Console.WriteLine($"Радіус (r): {radius:F3}");
             Console.WriteLine($"Кут (радіани): {angleRad:F3}");
--- lab_1/Vector.cs
+++ lab_1/Vector.cs
@@ -1,3 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace lab_1;
 
 public class Vector
