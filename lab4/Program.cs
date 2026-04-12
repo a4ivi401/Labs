1c810eba948f14cd072c453875d71a24070b89b5
Belinsky Alexander <a4ivi4.mail@gmail.com>
2026-04-12 21:35:54 +0300

    realize lab 4 on C#

---
+++ lab4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using lab4_lib;
+
+namespace lab4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringClasses.DigitalString myStr = new StringClasses.DigitalString("987654321");
+        
+        Console.WriteLine($"Початковий цифровий рядок: {myStr.Text}");
+        Console.WriteLine($"Довжина рядка: {myStr.GetLength()} символів");
+        
+        //reverse
+        myStr.ReverseString();
+        Console.WriteLine($"Обернений рядок: {myStr.Text}");
+        
+        StringClasses.DigitalString copiedStr = new StringClasses.DigitalString(myStr);
+        Console.WriteLine($"Скопійований рядок: {copiedStr.Text}");
+        
+        Console.ReadLine();
+        
+    }
+}
---
+++ lab4_lib/StringClasses.cs
@@ -0,0 +1,46 @@
+﻿namespace lab4_lib;
+
+public class StringClasses
+{
+    public class BaseString
+    {
+        public string Text { get; protected set; }
+        
+        public BaseString()
+        {
+            Text = "";
+        }
+        
+        public BaseString(string text)
+        {
+            Text = text;
+        }
+        
+        public BaseString(BaseString other)
+        {
+            Text = other.Text;
+        }
+        
+        public int GetLength()
+        {
+            return Text?.Length ?? 0;
+        }
+    }
+    public class DigitalString : BaseString
+    {
+        // Явний виклик базового конструктора за допомогою : base()
+        public DigitalString() : base() { }
+        public DigitalString(string text) : base(text) { }
+        public DigitalString(DigitalString other) : base(other) { }
+
+        // Метод обернення цифрового рядка
+        public void ReverseString()
+        {
+            if (string.IsNullOrEmpty(Text)) return;
+            
+            char[] charArray = Text.ToCharArray();
+            Array.Reverse(charArray);
+            Text = new string(charArray);
+        }
+    }
+}
